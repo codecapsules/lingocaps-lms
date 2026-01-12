593fc53ef9405d084af855d23de1d42cda7fd3a2
codecapsules <codecapsules@protonmail.com>
2026-01-13 00:39:50 +0100

    [pnpm] adding t3env for env gestion + some cfg on better-auth (api / auth-client and more)

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,5 @@
+import { env } from "./env";
+
 // lib/auth.ts
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
@@ -9,6 +11,12 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // ou "mysql", "sqlite", selon ton DB
   }),
+  socialProviders: {
+    github: {
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
+    },
+  },
   // Optionnel : config de session / callbacks selon BetterAuth
   session: {
     expiresIn: 30 * 24 * 60 * 60, // 30 jours
