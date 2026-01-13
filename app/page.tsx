45e4cd5c5d7f552f9fdbe7f15aceb0e81a1a2d96
codecapsules <codecapsules@protonmail.com>
2026-01-13 02:35:02 +0100

    [auth] adding better-auth + OAUTH for Github

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Homepage
+      {session ? <p>Logged as {session.user.email}</p> : <p>Not logged in</p>}
     </div>
   );
 }
