85d51f7c70002563da914094db65b4c549014f7d
codecapsules <codecapsules@protonmail.com>
2026-01-13 02:58:22 +0100

    [auth] adding authClient.signOut from better-auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,19 @@
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/components/auth/login-form";
 import Logo from "@/components/Logo";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("/");
+  }
+
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+"use client";
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function handleLogout() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login"); // redirect to login page
+          toast.success("Logged out successfully");
+        },
+      },
+    });
+  }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       {session ? <p>Logged as {session.user.email}</p> : <p>Not logged in</p>}
+      {session ? (
+        <Button variant="destructive" onClick={handleLogout}>
+          Logout
+        </Button>
+      ) : (
+        <p>No user ID</p>
+      )}
     </div>
   );
 }
